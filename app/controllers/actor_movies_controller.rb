b8c05965c163b8f23a6945a7a8b15993adbc8ed5
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-04 11:13:56 -0400

    add alternative to actor_movies_controller based on feedback

--- app/controllers/actor_movies_controller.rb
+++ app/controllers/actor_movies_controller.rb
@@ -2,6 +2,7 @@ class ActorMoviesController < ApplicationController
   def create
     actor_movie = ActorMovie.create!(actor_id: params[:actor_id], movie_id: params[:id])
     movie = Movie.find_by(id: actor_movie.movie_id)
+    # alternatively: movie = Movie.find_by(id: params[:id])
     redirect_to "/movies/#{movie.id}"
   end
 end
